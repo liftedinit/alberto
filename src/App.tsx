16da3d4267ae876efc4eedd3997c7f8953ce4385
Stanley Jones <stanley@sunshocked.com>
2021-12-16 23:29:13 -0800

    Basic setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import omni from "omni";
+import "./App.css";
+
+interface StatusResponse {
+  attributes: number[];
+}
+type InfoResponse = any[];
+
+const call =
+  (serverUrl: string, method: string, callback: (response: any) => void) =>
+  async () => {
+    const response = await omni.server.send(serverUrl, { method });
+    callback(response);
+  };
 
 function App() {
+  const [serverUrl, setServerUrl] = React.useState<string>("");
+  const [status, setStatus] = React.useState<StatusResponse | null>(null);
+  const [info, setInfo] = React.useState<InfoResponse | null>(null);
+  React.useEffect(() => {
+    const fetchStatus = call(serverUrl, "status", setStatus);
+    fetchStatus();
+    const fetchinfo = call(serverUrl, "ledger.info", setInfo);
+    fetchinfo();
+  }, [serverUrl]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        onChange={(event) => {
+          setServerUrl(event.currentTarget.value);
+        }}
+        defaultValue="http://localhost:8000/"
+      />
+      <h3>Attributes</h3>
+      {status ? (
+        <table>
+          <tbody>
+            <tr>
+              <td>Base</td>
+              <td>{`${status.attributes.includes(0) ? "🟢" : ""}`}</td>
+            </tr>
+            <tr>
+              <td>Blockchain</td>
+              <td>{`${status.attributes.includes(1) ? "🟢" : ""}`}</td>
+            </tr>
+            <tr>
+              <td>Ledger</td>
+              <td>{`${status.attributes.includes(2) ? "🟢" : ""}`}</td>
+            </tr>
+            <tr>
+              <td>ABCI-OMNI Bridge</td>
+              <td>{`${status.attributes.includes(1000) ? "🟢" : ""}`}</td>
+            </tr>
+          </tbody>
+        </table>
+      ) : (
+        "Loading"
+      )}
+      <h3>Symbols</h3>
+      {info ? (
+        <ul>
+          {info[0].map((item: string) => (
+            <li>{item}</li>
+          ))}
+        </ul>
+      ) : (
+        "Loading"
+      )}
     </div>
   );
 }
