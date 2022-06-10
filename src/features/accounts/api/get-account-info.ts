0a9a03aff68a77881d902015ae7e631f40a9115b
Tommy <tommy@theliftedinitiative.org>
2022-06-10 15:01:28 -0700

    fix import

--- src/features/accounts/api/get-account-info.ts
+++ src/features/accounts/api/get-account-info.ts
@@ -1,6 +1,6 @@
 import { useQuery } from "react-query"
 import { useNetworkContext } from "features/network"
-import { GetAccountInfoResponse } from "many-js"
+import type { GetAccountInfoResponse } from "many-js"
 
 export function useGetAccountInfo(address?: string) {
   const [n] = useNetworkContext()
