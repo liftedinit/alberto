9ed7bf688c4d2f306f0f98ea6525b97408b988df
Tommy <tommy@theliftedinitiative.org>
2022-07-13 13:28:32 -0700

    hide multisig settings if feature is not supported

--- src/views/accounts/account-details/account-details.tsx
+++ src/views/accounts/account-details/account-details.tsx
@@ -12,19 +12,27 @@ import { Assets } from "views/home/assets"
 import { TxnList } from "features/transactions"
 import { AccountInfo, useGetAccountInfo } from "features/accounts"
 import { MultisigSettings } from "../multisig-settings/multisig-settings"
+import { useNetworkStatus } from "features/network"
+import { AccountFeatureTypes, NetworkAttributes } from "many-js"
 
 enum TabNames {
   assets = "assets",
   activity = "activity",
-  multisigSettings = "multisig settings",
   accountSettings = "account settings",
+  multisigSettings = "multisig settings",
 }
-const tabs = Object.values(TabNames)
 
 export function AccountDetails() {
   const { accountAddress } = useParams()
+  const { getAttribute, getFeatures } = useNetworkStatus()
+  const accountAttribute = getAttribute(NetworkAttributes.account)
+  const { [AccountFeatureTypes.accountMultisig]: hasMultisig } = getFeatures(
+    accountAttribute,
+    [AccountFeatureTypes.accountMultisig],
+  )
 
   const [activeTabIdx, setActiveTabIdx] = React.useState(0)
+  let tabs = Object.values(TabNames).slice(0, hasMultisig ? undefined : -1)
   const activeTabName = tabs[activeTabIdx]
   const [, setContainerProps] = usePageContainerProvider()
   const { data } = useGetAccountInfo(accountAddress)
@@ -60,7 +68,7 @@ export function AccountDetails() {
             <MultisigSettings accountAddress={accountAddress} />
           )}
           {activeTabName === TabNames.accountSettings && (
-            <Box mt={4}>
+            <Box p={4}>
               <AccountInfo
                 accountInfo={data?.accountInfo}
                 address={accountAddress}
