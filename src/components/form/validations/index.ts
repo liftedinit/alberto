8626dbf755d78957362a0a7a79e7494a06a3efad
Tommy <tommy@theliftedinitiative.org>
2022-08-18 21:14:50 -0700

    minimum length of address should be 50

--- src/components/form/validations/index.ts
+++ src/components/form/validations/index.ts
@@ -1,7 +1,6 @@
 export const validateAddress = (val: string) => {
-  console.log({ formatVal: val })
   return (
-    (/^m[0-9a-z]*$/.test(val) && val.length === 50) ||
-    "Address should start with m and be 50 characters long"
+    (/^m[0-9a-z]*$/.test(val) && val.length >= 50) ||
+    "Address should start with m and have a minimum of 50 characters"
   )
 }
