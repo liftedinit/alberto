b34928118216b86236239af77cb39d0a879afc13
Tommy <tommy@theliftedinitiative.org>
2022-09-12 20:27:24 -0700

    shorten address text to show the first and last parts

--- src/components/form/validations/index.ts
+++ src/components/form/validations/index.ts
@@ -1,6 +1,8 @@
-export const validateAddress = (val: string) => {
+export const validateAddressMessage =
+  "Address should start with m and have a minimum of 50 characters"
+
+export function validateAddress(val: string): true | string {
   return (
-    (/^m[0-9a-z]*$/.test(val) && val.length >= 50) ||
-    "Address should start with m and have a minimum of 50 characters"
+    (/^m[0-9a-z]*$/.test(val) && val.length >= 50) || validateAddressMessage
   )
 }
