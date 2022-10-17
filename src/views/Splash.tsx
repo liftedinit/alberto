724e7667d2da8be0f35eb909cb7c179514d0efe0
Stanley Jones <stanley@sunshocked.com>
2022-10-17 14:42:26 -0700

    Style: Update logos, naming (#86)
    * Style: Update logos, naming
    
    * Fix: Put semicolons back
    
    * Fix: Never mind... remove semis again

--- src/views/Splash.tsx
+++ src/views/Splash.tsx
@@ -1,13 +1,13 @@
-import cube from "../assets/cube.png";
-import "./Splash.css";
+import cube from "../assets/cube.png"
+import "./Splash.css"
 
 function SplashView() {
   return (
     <div className="Splash" data-testid="splash-screen">
-      <img src={cube} alt="Albert Logo" />
+      <img src={cube} alt="Alberto Logo" />
       <h1>Alberto</h1>
       <h2>Lifted Initiative</h2>
     </div>
   )
 }
-export default SplashView;
+export default SplashView
