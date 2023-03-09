5d3435f90b87b19afe2350c91fd2bfb4418ded82
Stanley Jones <stanley@sunshocked.com>
2023-03-09 13:30:41 -0800

    fix: Disable flaky test, import Buffer polyfill

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -5,10 +5,13 @@
 import "@testing-library/jest-dom"
 import * as mediaQueryHooks from "@chakra-ui/media-query"
 import { TextEncoder, TextDecoder } from "util"
+import { Buffer } from "buffer"
 
 global.TextEncoder = TextEncoder
 // @ts-ignore
 global.TextDecoder = TextDecoder
+
+global.Buffer = Buffer
 
 //https://github.com/chakra-ui/chakra-ui/discussions/5558
 jest.spyOn(mediaQueryHooks, "useBreakpointValue")
