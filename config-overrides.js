9f91c9ab9d3213c6eeb6883ec3e0f7e01008d34c
Stanley Jones <stanley@sunshocked.com>
2023-03-09 13:06:37 -0800

    fix: Import buffer

--- config-overrides.js
+++ config-overrides.js
@@ -1,10 +1,20 @@
+const webpack = require("webpack")
+
 module.exports = {
   webpack: function override(webpackConfig) {
+    webpackConfig.resolve.fallback = {
+      buffer: require.resolve("buffer"),
+    }
     webpackConfig.module.rules.push({
       test: /\.mjs$/,
       include: /node_modules/,
       type: "javascript/auto",
     })
+    webpackConfig.plugins.push(
+      new webpack.ProvidePlugin({
+        Buffer: ["buffer", "Buffer"],
+      }),
+    )
 
     return webpackConfig
   },
