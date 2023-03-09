04035ab5ef3be146510ec89d796a0eb863ba6d10
Stanley Jones <stanley@sunshocked.com>
2023-03-09 13:35:30 -0800

    fix: Forgot we were still on Webpack 4

--- config-overrides.js
+++ config-overrides.js
@@ -1,20 +1,10 @@
-const webpack = require("webpack")
-
 module.exports = {
   webpack: function override(webpackConfig) {
-    webpackConfig.resolve.fallback = {
-      buffer: require.resolve("buffer"),
-    }
     webpackConfig.module.rules.push({
       test: /\.mjs$/,
       include: /node_modules/,
       type: "javascript/auto",
     })
-    webpackConfig.plugins.push(
-      new webpack.ProvidePlugin({
-        Buffer: ["buffer", "Buffer"],
-      }),
-    )
 
     return webpackConfig
   },
