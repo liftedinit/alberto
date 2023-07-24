f09c29bc5818869cf31c6fb8b684d7ab28a371c7
Felix C. Morency <1102868+fmorency@users.noreply.github.com>
2023-07-24 16:50:50 -0400

    test: wip

--- src/features/network/components/network-menu/__tests__/network-menu.test.tsx
+++ src/features/network/components/network-menu/__tests__/network-menu.test.tsx
@@ -65,33 +65,47 @@ describe("NetworkMenu", () => {
     userEvent.click(removeBtn)
     expect(screen.queryByText(/test-network/i)).not.toBeInTheDocument()
   })
-  // it("should edit a network", async () => {
-  //   const activeNetwork = await setupEditNetwork()
-  //   const nameInput = screen.getByLabelText(/name/i)
-  //   const urlInput = screen.getByLabelText(/url/i)
-  //   const saveBtn = screen.getByRole("button", { name: /save/i })
-  //   userEvent.type(nameInput, "-edited")
-  //   userEvent.type(urlInput, "-edited")
-  //
-  //   expect(nameInput).toHaveValue("Dummy-edited")
-  //   expect(urlInput).toHaveValue("/dummy-edited")
-  //   userEvent.click(saveBtn)
-  //   expect(within(activeNetwork).getByText(/dummy-edited/i)).toBeInTheDocument()
-  // })
+  it("should edit a network", async () => {
+    const activeNetwork = setupNetworkMenu()
+
+    // Create a new network
+    const addNewBtn = await screen.findAllByRole("button", {
+      name: /add network/i,
+    })
+    userEvent.click(addNewBtn[0])
+
+    const modal = screen.getByTestId("network-create-update-contents")
+    const saveBtn = within(modal).getByText(/save/i)
+    const form = within(modal).getByTestId("create-update-network-form")
+
+    const nameInput = within(form).getByLabelText(/name/i)
+    const urlInput = within(form).getByLabelText(/url/i)
+
+    userEvent.type(nameInput, "test-network")
+    userEvent.type(urlInput, "test-network/api")
+    userEvent.click(saveBtn)
+
+    // Edit network
+    const editBtn = (
+      await screen.findAllByRole("button", { name: /edit network/i })
+    )[0]
+    fireEvent.click(editBtn)
+    const newNameInput = screen.getByLabelText(/name/i)
+    const newUrlInput = screen.getByLabelText(/url/i)
+    const newSaveBtn = screen.getByRole("button", { name: /save/i })
+    userEvent.type(nameInput, "-edited")
+    userEvent.type(urlInput, "-edited")
+
+    expect(newNameInput).toHaveValue("Dummy-edited")
+    expect(newUrlInput).toHaveValue("/dummy-edited")
+    userEvent.click(newSaveBtn)
+    expect(within(activeNetwork).getByText(/dummy-edited/i)).toBeInTheDocument()
+  })
 })
 
 function setupNetworkMenu() {
   render(<NetworkMenu />)
   const activeNetwork = screen.getByLabelText("active network menu trigger")
   userEvent.click(activeNetwork)
-  return activeNetwork
-}
-
-async function setupEditNetwork() {
-  const activeNetwork = setupNetworkMenu()
-  const editBtn = (
-    await screen.findAllByRole("button", { name: /edit network/i })
-  )[1]
-  fireEvent.click(editBtn)
   return activeNetwork
 }
