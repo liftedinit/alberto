63615781f2ec37c32d7d461f15d40b8dfd0e92ec
Felix C. Morency <1102868+fmorency@users.noreply.github.com>
2023-07-24 16:58:50 -0400

    test: refactor

--- src/features/network/components/network-menu/__tests__/network-menu.test.tsx
+++ src/features/network/components/network-menu/__tests__/network-menu.test.tsx
@@ -1,104 +1,44 @@
 import {
+  cleanup,
+  fireEvent,
   render,
   screen,
   userEvent,
-  fireEvent,
   within,
-  waitForElementToBeRemoved,
 } from "test/test-utils"
 import { NetworkMenu } from "../network-menu"
 
 describe("NetworkMenu", () => {
+  afterEach(cleanup)
+
   it("should render with default manifest network", () => {
     render(<NetworkMenu />)
     expect(screen.getAllByText(/manifest/i).length).toBe(3)
   })
+
   it("should create a new network", async () => {
     const activeNetwork = setupNetworkMenu()
-    const addNewBtn = screen.getByText(/add network/i)
-    userEvent.click(addNewBtn)
-
-    const modal = screen.getByTestId("network-create-update-contents")
-    const saveBtn = within(modal).getByText(/save/i)
-    const form = within(modal).getByTestId("create-update-network-form")
-
-    const nameInput = within(form).getByLabelText(/name/i)
-    const urlInput = within(form).getByLabelText(/url/i)
-
-    userEvent.type(nameInput, "test-network")
-    userEvent.type(urlInput, "test-network/api")
-    userEvent.click(saveBtn)
-
+    createNetwork("test-network", "test-network/api")
     expect(within(activeNetwork).getByText("test-network")).toBeInTheDocument()
   })
+
   it("should remove a network", async () => {
     setupNetworkMenu()
 
-    // Create a new network
-    const addNewBtn = await screen.findAllByRole("button", {
-      name: /add network/i,
-    })
-    userEvent.click(addNewBtn[0])
-
-    const modal = screen.getByTestId("network-create-update-contents")
-    const saveBtn = within(modal).getByText(/save/i)
-    const form = within(modal).getByTestId("create-update-network-form")
-
-    const nameInput = within(form).getByLabelText(/name/i)
-    const urlInput = within(form).getByLabelText(/url/i)
-
-    userEvent.type(nameInput, "test-network")
-    userEvent.type(urlInput, "test-network/api")
-    userEvent.click(saveBtn)
-
-    // Remove the network
-    const editBtn = (
-      await screen.findAllByRole("button", { name: /edit network/i })
-    )[0]
-    fireEvent.click(editBtn)
-    const removeInput = screen.getByLabelText(/remove network/i)
-    const removeBtn = screen.getByTestId("remove network button")
-    expect(removeBtn).toBeDisabled()
-
-    userEvent.type(removeInput, "test-network/api")
-    expect(removeBtn).not.toBeDisabled()
-    userEvent.click(removeBtn)
+    // Create and remove a new network
+    createNetwork("test-network", "test-network/api")
+    await removeNetwork("test-network/api")
     expect(screen.queryByText(/test-network/i)).not.toBeInTheDocument()
   })
+
   it("should edit a network", async () => {
     const activeNetwork = setupNetworkMenu()
 
     // Create a new network
-    const addNewBtn = await screen.findAllByRole("button", {
-      name: /add network/i,
-    })
-    userEvent.click(addNewBtn[0])
-
-    const modal = screen.getByTestId("network-create-update-contents")
-    const saveBtn = within(modal).getByText(/save/i)
-    const form = within(modal).getByTestId("create-update-network-form")
-
-    const nameInput = within(form).getByLabelText(/name/i)
-    const urlInput = within(form).getByLabelText(/url/i)
-
-    userEvent.type(nameInput, "test-network")
-    userEvent.type(urlInput, "test-network/api")
-    userEvent.click(saveBtn)
+    createNetwork("test-network", "test-network/api")
 
     // Edit network
-    const editBtn = (
-      await screen.findAllByRole("button", { name: /edit network/i })
-    )[0]
-    fireEvent.click(editBtn)
-    const newNameInput = screen.getByLabelText(/name/i)
-    const newUrlInput = screen.getByLabelText(/url/i)
-    const newSaveBtn = screen.getByRole("button", { name: /save/i })
-    userEvent.type(nameInput, "-edited")
-    userEvent.type(urlInput, "-edited")
-
-    expect(newNameInput).toHaveValue("test-network-edited")
-    expect(newUrlInput).toHaveValue("test-network/api-edited")
-    userEvent.click(newSaveBtn)
+    await editNetwork("-edited", "-edited")
     expect(
       within(activeNetwork).getByText(/test-network-edited/i),
     ).toBeInTheDocument()
@@ -110,4 +50,50 @@ function setupNetworkMenu() {
   const activeNetwork = screen.getByLabelText("active network menu trigger")
   userEvent.click(activeNetwork)
   return activeNetwork
+}
+
+function createNetwork(name: string, url: string) {
+  const addNewBtn = screen.getByText(/add network/i)
+  userEvent.click(addNewBtn)
+
+  const { saveBtn, nameInput, urlInput } = getFormElements()
+
+  userEvent.type(nameInput, name)
+  userEvent.type(urlInput, url)
+  userEvent.click(saveBtn)
+}
+
+function getFormElements() {
+  const modal = screen.getByTestId("network-create-update-contents")
+  const saveBtn = within(modal).getByText(/save/i)
+  const form = within(modal).getByTestId("create-update-network-form")
+
+  const nameInput = within(form).getByLabelText(/name/i)
+  const urlInput = within(form).getByLabelText(/url/i)
+
+  return { saveBtn, nameInput, urlInput }
+}
+
+async function getEditButton() {
+  return (await screen.findAllByRole("button", { name: /edit network/i }))[0]
+}
+
+async function removeNetwork(removeInputValue: string) {
+  const editBtn = await getEditButton()
+  fireEvent.click(editBtn)
+  const removeInput = screen.getByLabelText(/remove network/i)
+  const removeBtn = screen.getByTestId("remove network button")
+  userEvent.type(removeInput, removeInputValue)
+  userEvent.click(removeBtn)
+}
+
+async function editNetwork(newName: string, newUrl: string) {
+  const editBtn = await getEditButton()
+  fireEvent.click(editBtn)
+  const newNameInput = screen.getByLabelText(/name/i)
+  const newUrlInput = screen.getByLabelText(/url/i)
+  const newSaveBtn = screen.getByRole("button", { name: /save/i })
+  userEvent.type(newNameInput, newName)
+  userEvent.type(newUrlInput, newUrl)
+  userEvent.click(newSaveBtn)
 }
