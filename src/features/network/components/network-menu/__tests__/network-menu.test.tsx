439cf4357ccf9865f41c6f2757e97873529b444c
Félix C. Morency <1102868+fmorency@users.noreply.github.com>
2023-07-25 09:46:46 -0400

    fix: prevent renaming the manifest ledger network (#121)

--- src/features/network/components/network-menu/__tests__/network-menu.test.tsx
+++ src/features/network/components/network-menu/__tests__/network-menu.test.tsx
@@ -1,61 +1,46 @@
 import {
+  cleanup,
+  fireEvent,
   render,
   screen,
   userEvent,
-  fireEvent,
   within,
-  waitForElementToBeRemoved,
 } from "test/test-utils"
 import { NetworkMenu } from "../network-menu"
 
 describe("NetworkMenu", () => {
+  afterEach(cleanup)
+
   it("should render with default manifest network", () => {
     render(<NetworkMenu />)
     expect(screen.getAllByText(/manifest/i).length).toBe(3)
   })
+
   it("should create a new network", async () => {
     const activeNetwork = setupNetworkMenu()
-    const addNewBtn = screen.getByText(/add network/i)
-    userEvent.click(addNewBtn)
-
-    const modal = screen.getByTestId("network-create-update-contents")
-    const saveBtn = within(modal).getByText(/save/i)
-    const form = within(modal).getByTestId("create-update-network-form")
-
-    const nameInput = within(form).getByLabelText(/name/i)
-    const urlInput = within(form).getByLabelText(/url/i)
-
-    userEvent.type(nameInput, "test-network")
-    userEvent.type(urlInput, "test-network/api")
-    userEvent.click(saveBtn)
-
+    await createNetwork("test-network", "test-network/api")
     expect(within(activeNetwork).getByText("test-network")).toBeInTheDocument()
   })
+
   it("should remove a network", async () => {
-    await setupEditNetwork()
-    const modal = screen.getByTestId("network-create-update-contents")
-    const removeInput = within(modal).getByLabelText(/remove network/i)
-    const removeBtn = within(modal).getByTestId("remove network button")
-    expect(removeBtn).toBeDisabled()
+    setupNetworkMenu()
 
-    userEvent.type(removeInput, "/api")
-    expect(removeBtn).not.toBeDisabled()
-    userEvent.click(removeBtn)
-    expect(screen.queryByText(/^manifest/i)).not.toBeInTheDocument()
+    // Create and remove a new network
+    await createNetwork("test-network", "test-network/api")
+    await removeNetwork("test-network/api")
+    expect(screen.queryByText(/test-network/i)).not.toBeInTheDocument()
   })
-  it("should edit a network", async () => {
-    const activeNetwork = await setupEditNetwork()
-    const nameInput = screen.getByLabelText(/name/i)
-    const urlInput = screen.getByLabelText(/url/i)
-    const saveBtn = screen.getByRole("button", { name: /save/i })
-    userEvent.type(nameInput, "-edited")
-    userEvent.type(urlInput, "-edited")
 
-    expect(nameInput).toHaveValue("Manifest Ledger-edited")
-    expect(urlInput).toHaveValue("/api-edited")
-    userEvent.click(saveBtn)
+  it("should edit a network", async () => {
+    const activeNetwork = setupNetworkMenu()
+
+    // Create a new network
+    await createNetwork("test-network", "test-network/api")
+
+    // Edit network
+    await editNetwork("-edited", "-edited")
     expect(
-      within(activeNetwork).getByText(/manifest ledger-edited/i),
+      within(activeNetwork).getByText(/test-network-edited/i),
     ).toBeInTheDocument()
   })
 })
@@ -67,11 +52,50 @@ function setupNetworkMenu() {
   return activeNetwork
 }
 
-async function setupEditNetwork() {
-  const activeNetwork = setupNetworkMenu()
-  const editBtn = (
-    await screen.findAllByRole("button", { name: /edit network/i })
-  )[0]
+async function createNetwork(name: string, url: string) {
+  const addNewBtn = await screen.findAllByRole("button", {
+    name: /add network/i,
+  })
+  userEvent.click(addNewBtn[0])
+
+  const { saveBtn, nameInput, urlInput } = getFormElements()
+
+  userEvent.type(nameInput, name)
+  userEvent.type(urlInput, url)
+  userEvent.click(saveBtn)
+}
+
+function getFormElements() {
+  const modal = screen.getByTestId("network-create-update-contents")
+  const saveBtn = within(modal).getByText(/save/i)
+  const form = within(modal).getByTestId("create-update-network-form")
+
+  const nameInput = within(form).getByLabelText(/name/i)
+  const urlInput = within(form).getByLabelText(/url/i)
+
+  return { saveBtn, nameInput, urlInput }
+}
+
+async function getEditButton() {
+  return (await screen.findAllByRole("button", { name: /edit network/i }))[0]
+}
+
+async function removeNetwork(removeInputValue: string) {
+  const editBtn = await getEditButton()
   fireEvent.click(editBtn)
-  return activeNetwork
+  const removeInput = screen.getByLabelText(/remove network/i)
+  const removeBtn = screen.getByTestId("remove network button")
+  userEvent.type(removeInput, removeInputValue)
+  userEvent.click(removeBtn)
+}
+
+async function editNetwork(newName: string, newUrl: string) {
+  const editBtn = await getEditButton()
+  fireEvent.click(editBtn)
+  const newNameInput = screen.getByLabelText(/name/i)
+  const newUrlInput = screen.getByLabelText(/url/i)
+  const newSaveBtn = screen.getByRole("button", { name: /save/i })
+  userEvent.type(newNameInput, newName)
+  userEvent.type(newUrlInput, newUrl)
+  userEvent.click(newSaveBtn)
 }
