de373d2764b4e8455d9ca09b1cd8d9e09481ab37
Félix C. Morency <1102868+fmorency@users.noreply.github.com>
2023-10-12 11:01:16 -0400

    fix: hsm account creation regression (#147)

--- src/features/accounts/api/save-webauthn-credential.ts
+++ src/features/accounts/api/save-webauthn-credential.ts
@@ -1,9 +1,9 @@
 import { useMutation } from "react-query"
 import { useNetworkContext } from "features/network"
-import { WebAuthnIdentity } from "@liftedinit/many-js"
+import { IdStore, Network, WebAuthnIdentity } from "@liftedinit/many-js"
 
 export function useSaveWebauthnCredential() {
-  const { command: n } = useNetworkContext()
+  const { command } = useNetworkContext()
   return useMutation<
     { phrase: string },
     Error,
@@ -13,7 +13,10 @@ export function useSaveWebauthnCredential() {
       cosePublicKey: ArrayBuffer
       identity: WebAuthnIdentity
     }
-  >(async ({ address, credentialId, cosePublicKey }) => {
+  >(async ({ address, credentialId, cosePublicKey, identity }) => {
+    // This is required because at this point the Webauthn identity is not yet stored in the store.
+    const n = new Network(command?.url ?? "", identity)
+    n.apply([IdStore])
     return await n?.idStore.store(address, credentialId, cosePublicKey)
   })
 }
