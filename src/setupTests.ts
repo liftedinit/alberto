dcd9d931c609cbf1543e7c97e71ec44c60b6c278
Félix C. Morency <1102868+fmorency@users.noreply.github.com>
2025-03-24 11:52:52 -0400

    chore: bump web3auth to 9.7.0 (#169)

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,6 +2,8 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
+require("./test/web3authMock")
+
 import "@testing-library/jest-dom"
 import * as mediaQueryHooks from "@chakra-ui/media-query"
 import { TextEncoder, TextDecoder } from "util"
