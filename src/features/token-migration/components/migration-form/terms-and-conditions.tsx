9ed5d552f94329eff43b4ec06826e1e1c4b2829f
Felix C. Morency <1102868+fmorency@users.noreply.github.com>
2025-04-01 09:50:59 -0400

    chore: .

--- src/features/token-migration/components/migration-form/terms-and-conditions.tsx
+++ src/features/token-migration/components/migration-form/terms-and-conditions.tsx
@@ -4,6 +4,7 @@ import content from "../../assets/terms-and-conditions_migration.md"
 import { marked } from "marked"
 
 export const TokenMigrationTermsAndConditions: React.FC = () => {
+  console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
   console.log("Content:", content)
   const htmlContent = marked(content, { async: false })
 
