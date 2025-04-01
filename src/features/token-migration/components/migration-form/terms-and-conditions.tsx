8971021fc24eff87191aa96f72d808076f49eef9
Felix C. Morency <1102868+fmorency@users.noreply.github.com>
2025-04-01 09:23:43 -0400

    chore: work

--- src/features/token-migration/components/migration-form/terms-and-conditions.tsx
+++ src/features/token-migration/components/migration-form/terms-and-conditions.tsx
@@ -1,10 +1,11 @@
 import React from "react"
 import { Box } from "@liftedinit/ui"
-import termsContent from "../../assets/terms-and-conditions_migration.md"
+import content from "../../assets/terms-and-conditions_migration.md"
 import { marked } from "marked"
 
 export const TokenMigrationTermsAndConditions: React.FC = () => {
-  const htmlContent = marked(termsContent, { async: false })
+  console.log("Content:", content)
+  const htmlContent = marked(content, { async: false })
 
   return (
     <Box maxH="500px" overflowY="auto" p={4}>
