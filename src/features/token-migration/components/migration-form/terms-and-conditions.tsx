ffc17123679c74a26844673229be0938adde7028
Felix C. Morency <1102868+fmorency@users.noreply.github.com>
2025-04-01 09:04:51 -0400

    fix: md

--- src/features/token-migration/components/migration-form/terms-and-conditions.tsx
+++ src/features/token-migration/components/migration-form/terms-and-conditions.tsx
@@ -1,11 +1,14 @@
 import React from "react"
 import { Box } from "@liftedinit/ui"
 import termsContent from "../../assets/terms-and-conditions_migration.md"
+import { marked } from "marked"
 
 export const TokenMigrationTermsAndConditions: React.FC = () => {
+  const htmlContent = marked(termsContent, { async: false })
+
   return (
     <Box maxH="500px" overflowY="auto" p={4}>
-      <div dangerouslySetInnerHTML={{ __html: termsContent }} />
+      <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
     </Box>
   )
 }
