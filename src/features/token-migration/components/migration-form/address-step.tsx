af75c93ef632fe445330c04e0fb639bf1ed11f28
Felix C. Morency <1102868+fmorency@users.noreply.github.com>
2025-04-07 14:37:45 -0400

    chore: wrong step

--- src/features/token-migration/components/migration-form/address-step.tsx
+++ src/features/token-migration/components/migration-form/address-step.tsx
@@ -11,6 +11,7 @@ import {
 import { useEffect, useMemo, useState } from "react"
 import { IdentitiesAndAccounts, IdTypes, StepNames } from "./types"
 import { useCombinedAccountInfo } from "features/accounts/queries"
+import { useMigrationWhitelist } from "../../queries"
 
 interface FormValues {
   address: string
@@ -51,11 +52,15 @@ export const AddressStep = ({
     () => combinedAccountAndIdentities,
     [combinedAccountAndIdentities],
   )
+  const { whitelist, isLoading } = useMigrationWhitelist()
 
   useEffect(() => {
+    if (isLoading) {
+      return
+    }
     setAccountsAndIdentities(memoizedCombinedAccountAndIdentities)
     setIsLoaded(true)
-  }, [memoizedCombinedAccountAndIdentities])
+  }, [memoizedCombinedAccountAndIdentities, isLoading])
 
   const handleSubmit = (values: FormValues) => {
     const addressType = detectAddressType(values.address)
@@ -101,16 +106,26 @@ export const AddressStep = ({
               >
                 {isLoaded
                   ? Array.from(accountsAndIdentities.values()).map(
-                      ({ idType, address, name }) => (
-                        <option
-                          key={address}
-                          value={address}
-                          data-testid="form-option"
-                        >
-                          {idType === IdTypes.USER ? "User" : "Account"}:{" "}
-                          {address} {name ? `(${name})` : null}
-                        </option>
-                      ),
+                      ({ idType, address, name }) => {
+                        const isWhitelisted =
+                          !isLoading && whitelist?.includes(address)
+                        return (
+                          <option
+                            key={address}
+                            value={address}
+                            data-testid="form-option"
+                            disabled={!isWhitelisted}
+                          >
+                            {idType === IdTypes.USER ? "User" : "Account"}:{" "}
+                            {address} {name ? `(${name})` : null}
+                            {isWhitelisted ? null : (
+                              <Text color="red.500" fontSize="sm">
+                                (Not whitelisted)
+                              </Text>
+                            )}
+                          </option>
+                        )
+                      },
                     )
                   : null}
               </Field>
