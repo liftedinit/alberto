b018eb7acadf020123181d1815100a4f463795e8
Felix C. Morency <1102868+fmorency@users.noreply.github.com>
2025-06-10 13:41:49 -0400

    fix: remove migration banner

--- src/views/layout/app-nav.tsx
+++ src/views/layout/app-nav.tsx
@@ -1,13 +1,13 @@
 import { Flex } from "@liftedinit/ui"
 import { NetworkMenu } from "features/network"
 import { AccountsMenu } from "features/accounts"
-import { MigrationBanner } from "../../features/token-migration"
+// import { MigrationBanner } from "../../features/token-migration"
 
 export function AppNav() {
   return (
     <Flex justify="space-between" alignItems="center" p={2} overflow="hidden">
       <AccountsMenu />
-      <MigrationBanner />
+      {/*<MigrationBanner />*/}
       <NetworkMenu />
     </Flex>
   )
